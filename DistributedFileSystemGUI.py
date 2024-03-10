5408024a587f5a87fa12cc0b001edac190cab90e
jcDev7923 <jchan3132@proton.me>
2024-03-10 16:51:12 -0400

    Added GUI files to repository

---
+++ DistributedFileSystemGUI.py
@@ -0,0 +1,129 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import socket
+import json
+import subprocess  
+from Crypto.PublicKey import RSA
+import base64
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+def choose_directory():
+    directory = filedialog.askdirectory()
+    directory_entry.delete(0, tk.END)  # Clear any previous value
+    directory_entry.insert(0, directory)
+
+def choose_monitor_directory():
+    directory = filedialog.askdirectory()
+    folderToMonitorEntry.delete(0, tk.END)  # Clear any previous value
+    folderToMonitorEntry.insert(0, directory)
+
+
+def choose_file():
+    file_path = filedialog.askopenfilename()
+    file_entry.delete(0, tk.END)  # Clear any previous value
+    file_entry.insert(0, file_path)
+
+def searchAndDownloadFile(filename, downloadDirectory):
+    if not os.path.exists(downloadDirectory):
+        messagebox.showinfo("ERROR",f"The directory '{downloadDirectory}' does not exist.")
+        return
+    # localhost: '127.0.0.1'
+    server_host = '35.224.31.170' # Server's IP address
+    server_port = 65420           # Server's port
+    key_pair = RSA.generate(2048) # Generate new RSA keys
+    public_key = key_pair.publickey().exportKey() 
+    public_key_decoded = public_key.decode('utf-8')
+    private_key = key_pair.exportKey() 
+    private_key_encoded = base64.urlsafe_b64encode(private_key).decode('utf-8')
+
+    # Establish connection to the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_host, server_port))
+        client_socket.sendall(b'client')  # Identify this client as a 'client', not a 'seeder'
+
+        message = json.dumps({'type': 'SEARCH', 'filename': filename})
+        client_socket.sendall(message.encode('utf-8'))
+
+        response = client_socket.recv(1024).decode('utf-8')
+        response_data = json.loads(response)
+
+        if response_data['exists']:
+            filesize = response_data['filesize']
+            print(f"File exists on server with size {filesize} bytes.")
+            result = messagebox.askyesno("File Found", f"The file is located in the server and it's size is {filesize} bytes. Would you like to download it?")
+            if result:
+                download_message = json.dumps({'type': 'DOWNLOAD', 'filename': filename, 'public_key': public_key_decoded})
+                client_socket.sendall(download_message.encode('utf-8'))
+                transfer_response = client_socket.recv(1024).decode('utf-8')
+                transfer_data = json.loads(transfer_response)
+                
+                if transfer_data['type'] == 'TRANSFER':
+                    session_info = transfer_data['session']
+                    session_id = session_info['session_id']
+                    seeder_public_key = session_info['public_key']
+                    
+                    seeder_public_key_encoded = base64.urlsafe_b64encode(seeder_public_key.encode('utf-8')).decode('utf-8')
+
+                    download_file = downloadDirectory + '/' + filename
+                    print(f"Starting download with session ID {session_id}.")
+                    # For Windows:
+                    subprocess.Popen(['start', 'cmd', '/k', 'python', 'leecher.py', str(session_id), str(download_file), private_key_encoded, seeder_public_key_encoded], shell=True)
+        else:
+            messagebox.showinfo("Error", "File not found in the system...")
+
+def toggleFileMonitor(monitorDirectory):
+    if not os.path.exists(monitorDirectory):
+        messagebox.showinfo("ERROR",f"The directory '{monitorDirectory}' does not exist.")
+        return    
+    subprocess.Popen(['start', 'cmd', '/k', 'python', 'folder_monitor_gui.py', monitorDirectory], shell=True)
+
+# Create the main Tkinter window
+root = tk.Tk()
+root.title("Distributed File System Client GUI")
+root.resizable(False, False)  # Disable window resizing
+# Title
+title_label = tk.Label(root, text="Distributed File System Client", font=("Helvetica", 16, "bold"))
+title_label.grid(row=0, columnspan=3, padx=10, pady=10)
+
+# Directory selection
+directory_label = tk.Label(root, text="Download Location:")
+directory_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+directory_entry = tk.Entry(root, width=50)
+directory_entry.grid(row=1, column=1, padx=10, pady=10)
+
+directory_button = tk.Button(root, text="Search Directory", command=choose_directory)
+directory_button.grid(row=1, column=2, padx=10, pady=10)
+
+# File selection
+file_label = tk.Label(root, text="File to search:")
+file_label.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
+
+file_entry = tk.Entry(root, width=50)
+file_entry.grid(row=2, column=1, padx=10, pady=10)
+
+file_button = tk.Button(root, text="Search File", command=lambda: searchAndDownloadFile(file_entry.get(), directory_entry.get()))
+file_button.grid(row=2, column=2, padx=10, pady=10)
+
+#File Monitor Components
+title_label = tk.Label(root, text="Folder Monitor", font=("Helvetica", 10, "bold"))
+title_label.grid(row=3, columnspan=3, padx=10, pady=10)
+
+folderToMonitorLabel = tk.Label(root, text="File to monitor:")
+folderToMonitorLabel.grid(row=4, column=0, padx=10, pady=10, sticky=tk.W)
+
+folderToMonitorEntry = tk.Entry(root, width=50)
+folderToMonitorEntry.grid(row=4, column=1, padx=10, pady=10)
+
+folderToMonitorButton = tk.Button(root, text="Select File to Monitor", command=choose_monitor_directory)
+folderToMonitorButton.grid(row=4, column=2, padx=10, pady=10)
+
+activateMonitorButton = tk.Button(root, text="Launch Monitor", command=lambda: toggleFileMonitor(folderToMonitorEntry.get()))
+activateMonitorButton.grid(row=5, column=2, padx=10, pady=10)
+
+root.mainloop()
+
+
---
+++ folder_monitor_gui.py
@@ -0,0 +1,140 @@
+# folder_monitor.py 
+
+import sys  # Import sys module to access command-line arguments
+import time
+import socket
+import json
+import os  
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import subprocess 
+import threading
+from Crypto.PublicKey import RSA
+import base64
+
+class Watcher:
+    def __init__(self, server_address, directory_to_watch):
+        self.observer = Observer()
+        self.server_address = server_address
+        self.client_socket = None
+        self.directory_to_watch = directory_to_watch  # Store the directory to watch
+        self.key_pair = RSA.generate(2048)  # Generate new RSA keys
+        self.public_key = self.key_pair.publickey().exportKey() 
+        self.private_key = self.key_pair.exportKey()    
+        self.private_key_encoded = base64.urlsafe_b64encode(self.private_key).decode('utf-8')
+
+    def connect_to_server(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(self.server_address)
+        # Send an initial message to identify this client as a 'seeder'
+        self.client_socket.sendall(b'seeder')  # Sending 'seeder' as bytes
+        time.sleep(0.5)  # Wait for half a second
+
+    def run(self):
+        print("Starting the watcher...")
+        self.connect_to_server()  # Establish connection to the server
+
+        # Start listening for instructions in a new thread after connection is established
+        instructions_thread = threading.Thread(target=self.listen_for_instructions)
+        instructions_thread.start()
+
+        event_handler = Handler(self.client_socket, self.public_key)
+        self.send_initial_files(event_handler)
+        self.observer.schedule(event_handler, self.directory_to_watch, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except KeyboardInterrupt:
+            self.observer.stop()
+            print("Observer Stopped")
+            self.client_socket.close()  # Close the connection when done
+
+        self.observer.join()
+    
+    def send_initial_files(self, handler):
+        for root, dirs, files in os.walk(self.directory_to_watch):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                handler.send_message('ADD', filepath)
+                time.sleep(0.1)  # Add a short delay between sends
+
+    def handle_transfer_instruction(self, session_id, file_path, leecher_public_key_encoded):
+        # Start the new process
+        print(f"Starting a new process for {file_path} with session ID {session_id}...")
+        subprocess.Popen(['start', 'cmd', '/k', 'python', 'seeder.py', str(session_id), str(file_path), self.private_key_encoded, leecher_public_key_encoded], shell=True)
+    
+    def listen_for_instructions(self):
+        print("Listening for instructions from the server...")
+        try:
+            while True:
+                data = self.client_socket.recv(1024).decode('utf-8')
+                if not data:
+                    break  # No more data from the server
+
+                message = json.loads(data)
+                if message['type'] == 'TRANSFER':
+                    print("Received transfer instruction from the server.")
+                    # Extract the session information
+                    session_info = message['session']
+                    session_id = session_info['session_id']
+                    file_path = session_info['file_path']  
+                    leecher_public_key = session_info['public_key'] 
+                    leecher_public_key_encoded = base64.urlsafe_b64encode(leecher_public_key.encode('utf-8')).decode('utf-8')
+
+                    # Pass the session ID and file path to the method that handles the transfer
+                    self.handle_transfer_instruction(session_id, file_path, leecher_public_key_encoded)
+        except Exception as e:
+            print(f"Error while listening for instructions: {e}")
+        finally:
+            self.client_socket.close()
+
+class Handler(FileSystemEventHandler):
+    def __init__(self, client_socket, public_key):
+        self.client_socket = client_socket
+        self.public_key = public_key    
+        self.public_key_decoded = public_key.decode('utf-8')    
+
+    def send_message(self, action, filepath):
+        # Extract filename and size from filepath
+        filename = os.path.basename(filepath)
+        if action == 'ADD':
+            filesize = os.path.getsize(filepath)  # Get the size of the file
+        else:
+            filesize = 0
+        # Constructing and sending JSON formatted message
+        message = json.dumps({
+            'type': action,
+            'filename': filename,
+            'filelocation': filepath,
+            'filesize': filesize,  # Add the file size here
+            'public_key': self.public_key_decoded
+
+        }) + '\n'
+        self.client_socket.sendall(message.encode('utf-8'))
+
+    def on_created(self, event):
+        if not event.is_directory:
+            self.send_message('ADD', event.src_path)
+
+    def on_deleted(self, event):
+        if not event.is_directory:
+            self.send_message('DELETE', event.src_path)
+
+    def on_moved(self, event):
+        if not event.is_directory:
+            # Send DELETE for old location and ADD for new location
+            self.send_message('DELETE', event.src_path)
+            self.send_message('ADD', event.dest_path)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python folder_monitor.py <directory_to_watch>")
+        sys.exit(1)
+    
+    directory_to_watch = sys.argv[1]
+    # localhost: '127.0.0.1'
+    SERVER_HOST = '35.224.31.170'     # Server's IP address
+    SERVER_PORT = 65420               # Server's port
+    w = Watcher((SERVER_HOST, SERVER_PORT), directory_to_watch)
+    w.run()
